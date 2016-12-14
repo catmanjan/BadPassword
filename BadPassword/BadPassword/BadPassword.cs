892d29a5be70bea4e3912cab15b78e60ba3630d6
Jan Martin <jan.ramon.martin@gmail.com>
2016-12-15 09:03:28 +1100

    created project

---
+++ BadPassword/BadPassword/BadPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadPassword
+{
+    public static class BadPassword
+    {
+        /// <summary>
+        /// Check a string against a compiled index of common passwords. 
+        /// </summary>
+        /// <param name="password">Plain-text unencrypted user entered password.</param>
+        /// <returns>True if</returns>
+        public static bool IsBadPassword(this string password)
+        {
+            return false;
+        }
+    }
+}
